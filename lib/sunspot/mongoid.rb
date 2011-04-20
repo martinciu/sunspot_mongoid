02582b8704ffdd696ac35ced21c8c99f35b0042d
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2011-04-20 13:26:52 +0200

    repair_index added

--- lib/sunspot/mongoid.rb
+++ lib/sunspot/mongoid.rb
@@ -22,22 +22,35 @@ module Sunspot
         Sunspot::Adapters::DataAccessor.register(DataAccessor, base)
         Sunspot::Adapters::InstanceAdapter.register(InstanceAdapter, base)
         
-        def self.solr_index_orphans
-          count = self.count
-          indexed_ids = solr_search_ids { paginate(:page => 1, :per_page => count) }.to_set
-          only(:id).each do |object|
-            indexed_ids.delete(object.id.to_s)
-          end
-          indexed_ids.to_a
-        end
-        
         def self.solr_execute_search_ids(options = {})
           search = yield
           search.raw_results.map { |raw_result| raw_result.primary_key }
         end
         
         def self.solr_clean_index_orphans
-          Sunspot.remove_by_id!(self.class, solr_index_orphans)
+          solr_index_diff[:orphans].each do |id|
+            Sunspot.remove_by_id(self.class, id)
+          end
+          Sunspot.commit
+        end
+        
+        def self.solr_reindex_missing
+          Sunspot.index!(find(solr_index_diff[:missing]))
+        end
+        
+        def self.solr_index_diff
+          indexed_ids = solr_search_ids { paginate(:page => 1, :per_page => (self.count*10)) }
+          all_ids = only(:id).map {|d| d.id.to_s}
+          missing = all_ids - indexed_ids
+          orphans = indexed_ids - all_ids
+          return {:missing => missing, :orphans => orphans}
+        end
+        
+        def self.solr_repair_index
+          diff = solr_index_diff
+          diff[:orphans].each {|id| Sunspot.remove_by_id(self.class, id) }
+          Sunspot.index(find(diff[:missing]))
+          Sunspot.commit
         end
         
         
